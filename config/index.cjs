d04711aa795897b17ad3342e1e96b6cd653345d3
lanxi637 <113596036+lanxi637@users.noreply.github.com>
2022-12-12 23:27:41 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -13,7 +13,7 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '2f746925ca3f6fec5e50a5016602e8a2',
 
-  PROVINCE: '贵州',
+  PROVINCE: '贵州省',
   CITY: '望谟县',
 
   USERS: [
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oThCa5tJ-ZuY_WILCCl0mHnUhQDQ',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'r3rR2o1rrFDulfhiw86W8YI93EtS7eDu5_SP1vbCdYM',
+      useTemplateId: '63XvaVA3SjQr0Yd65YwAwIGAkgHGqGfshMju5wS_aUY',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '05-01',
       festivals: [
