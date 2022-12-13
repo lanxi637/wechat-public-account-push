e7cb44d71eb8f15be205886cae3298bbc442d1dd
lanxi637 <113596036+lanxi637@users.noreply.github.com>
2022-12-13 12:26:43 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -44,7 +44,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oThCa5tJ-ZuY_WILCCl0mHnUhQDQ',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'FCGGplAYEYdJh7-S5I50-Oe1TG1Csqgx8tNnlq5czDI',
+      useTemplateId: 'DWD3GPMtN0_Iwp7mZtr_UiX2XyBBpDzehzhWd9Y3cJI',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '05-01',
       festivals: [
@@ -71,7 +71,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '9jMO9VWiBChiYJSGkOZS4eq88Zkrng-n9eYgaetZ0yk',
+  CALLBACK_TEMPLATE_ID: 'DWD3GPMtN0_Iwp7mZtr_UiX2XyBBpDzehzhWd9Y3cJI',
 
   CALLBACK_USERS: [
     {
