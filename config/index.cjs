39c2cd9ac497d670e5305778678e976cfd75bd04
lanxi637 <113596036+lanxi637@users.noreply.github.com>
2022-12-13 14:38:42 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -40,13 +40,9 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '念念宝贝',
-      // 想要发送的人的名字
-      name: '我',
+      name: '念念宝贝','我',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oThCa5k-4b7Q0S3mcowRUxqZofgA',
-      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oThCa5tJ-ZuY_WILCCl0mHnUhQDQ',
+      id: 'oThCa5k-4b7Q0S3mcowRUxqZofgA','oThCa5tJ-ZuY_WILCCl0mHnUhQDQ',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'ZvWmMqKAPjE5SPcpVCPzl8lq5OeBKGAoY6J-ygQkZu8',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
